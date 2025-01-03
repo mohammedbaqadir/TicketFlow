5f76acfc7a87d1dcfe2bb27a2991b47c16a66387
Mohammed Baqadir <mohammed.baqadir@outlook.com>
2025-01-03 22:47:22 +0300

    refactor(config): Add ticket grouping configuration file

---
+++ config/tickets.php
@@ -0,0 +1,50 @@
+<?php
+    declare( strict_types = 1 );
+
+    return [
+        'groupings' => [
+            'index' => [
+                [
+                    'title' => 'Working On It',
+                    'status' => [ 'in-progress', 'awaiting-acceptance' ],
+                    'assignee_required' => true,
+                    'no_tickets_msg' => 'You Are Not Currently Working on Any Tickets'
+                ],
+                [
+                    'title' => 'Un-Assigned Tickets',
+                    'status' => [ 'open' ],
+                    'assignee_required' => false,
+                    'no_tickets_msg' => 'There are No Un-Assigned Tickets'
+                ],
+                [
+                    'title' => 'Resolved It',
+                    'status' => [ 'resolved' ],
+                    'assignee_required' => true,
+                    'no_tickets_msg' => 'You did NOT Resolve Any Tickets Yet'
+                ],
+                [
+                    'title' => 'Resolved',
+                    'status' => [ 'resolved' ],
+                    'assignee_required' => false,
+                    'no_tickets_msg' => 'There are No Resolved Tickets'
+                ]
+            ],
+            'my_tickets' => [
+                [
+                    'title' => 'Pending Action',
+                    'status' => [ 'awaiting-acceptance' ],
+                    'no_tickets_msg' => 'no tickets are pending action from you'
+                ],
+                [
+                    'title' => 'On-Going',
+                    'status' => [ 'open', 'in-progress', 'elevated' ],
+                    'no_tickets_msg' => 'you do not have ongoing tickets'
+                ],
+                [
+                    'title' => 'Resolved',
+                    'status' => [ 'resolved' ],
+                    'no_tickets_msg' => 'you do not have any closed tickets yet'
+                ]
+            ]
+        ]
+    ];
